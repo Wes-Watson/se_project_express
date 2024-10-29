2b9e51419b3a28ef83c451f7d394a15d919ea72d
Wes Watson <wesrwats@gmail.com>
2024-10-29 14:13:12 -0500

    Initial Commit

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+console.log({_id : ""});
+
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: ["eslint:recommended", "airbnb-base", "prettier"],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: { "no-underscore-dangle": "off"},
+};
