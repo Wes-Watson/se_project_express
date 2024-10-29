386710f0b78e6722530250a875cb2c07b1152cc9
Wes Watson <wesrwats@gmail.com>
2024-10-29 14:30:09 -0500

    Add Database

--- app.js
+++ app.js
@@ -2,8 +2,17 @@ const express = require("express");
 
 const app = express();
 
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/wtwr_db")
+  .then(() => {
+    console.log("connected to DB");
+  })
+  .catch(console.error);
+
 const { PORT = 3001 } = process.env;
 
 app.listen(PORT, () => {
   console.log(`App listening at port ${PORT}`);
-})
+});
